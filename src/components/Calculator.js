270c87119565989973dc0354a08ec28c0e4fdced
Neto <netod90@gmail.com>
2023-02-15 11:12:32 -0700

    Add basic calculator scss

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,40 @@
-import './Calculator.css';
+import './Calculator.scss';
 
 const Calculator = () => (
-  <section>
+  <section id="calculator">
     <table>
-      <thead>
-        <tr>
-          <th>0</th>
-        </tr>
-      </thead>
       <tbody>
-        <tr>
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td>/</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td>x</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td>-</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td>+</td>
-        </tr>
-        <tr>
+        <tr className="result">
           <td>0</td>
-          <td>.</td>
-          <td>*</td>
+        </tr>
+        <tr className="buttons-row">
+          <td><button type="button">AC</button></td>
+          <td><button type="button">+/-</button></td>
+          <td><button type="button">%</button></td>
+          <td><button type="button" className="orange">/</button></td>
+        </tr>
+        <tr className="buttons-row">
+          <td><button type="button">7</button></td>
+          <td><button type="button">8</button></td>
+          <td><button type="button">9</button></td>
+          <td><button type="button" className="orange">x</button></td>
+        </tr>
+        <tr className="buttons-row">
+          <td><button type="button">4</button></td>
+          <td><button type="button">5</button></td>
+          <td><button type="button">6</button></td>
+          <td><button type="button" className="orange">-</button></td>
+        </tr>
+        <tr className="buttons-row">
+          <td><button type="button">1</button></td>
+          <td><button type="button">2</button></td>
+          <td><button type="button">3</button></td>
+          <td><button type="button" className="orange">+</button></td>
+        </tr>
+        <tr className="buttons-last-row">
+          <td><button type="button">0</button></td>
+          <td><button type="button">.</button></td>
+          <td><button type="button" className="orange">*</button></td>
         </tr>
       </tbody>
     </table>
